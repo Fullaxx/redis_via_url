9958f16904dd0456f8e2962b451cb97c4471813c
Brett Kuskie <fullaxx@gmail.com>
2024-10-07 22:14:30 +0000

    small update

--- test.c
+++ test.c
@@ -2,32 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-static void redis_url_decode(char *buf, int len)
+static int validate_url_rediss(char *buf, int len)
+{
+	return 1;
+}
+
+static int validate_url_redis(char *buf, int len)
+{
+	return 1;
+}
+
+static int validate_url_unix(char *buf, int len)
 {
 	char *options = NULL;
 	char *username = NULL;
 	printf("%s\n", buf);
-
-//	Decode Protocol
-	if (strncmp(buf, "unix://", 7) == 0) {
-		printf("protocol: unix\n");
-		buf += 7; len -= 7;
-	} else if (strncmp(buf, "redis://", 8) == 0) {
-		printf("protocol: redis\n");
-		buf += 8; len -= 8;
-	} else if (strncmp(buf, "rediss://", 9) == 0) {
-		printf("protocol: rediss\n");
-		buf += 9; len -= 9;
-	} else {
-		printf("ERROR\n");
-		return;
-	}
 
 //	Look for question mark
 	char *qm = strchr(buf, '?');
 	if (qm) {
 		*qm = 0;
 		options = qm+1;
+		if(strlen(options) < 3) { printf("options not valid!\n"); return 1; }
 	}
 
 //	Look for username
@@ -37,12 +33,38 @@ static void redis_url_decode(char *buf, int len)
 		username = buf;
 		buf = at+1;
 		len = strlen(buf);
+		if(strlen(username) < 1) { printf("username not valid!\n"); return 1; }
 	}
+
+	if(strlen(buf) < 1) { printf("location not valid!\n"); return 1; }
 
 	if(username) { printf("username: %s\n", username); }
 	printf("location: %s\n", buf);
 	if(options) { printf("options: %s\n", options); }
 	printf("\n");
+
+	return 1;
+}
+
+static int validate_url(char *buf, int len)
+{
+//	Check Scheme
+	if (strncmp(buf, "unix://", 7) == 0) {
+		printf("scheme: unix\n");
+		buf += 7; len -= 7;
+		return validate_url_unix(buf, len);
+	} else if (strncmp(buf, "redis://", 8) == 0) {
+		printf("scheme: redis\n");
+		buf += 8; len -= 8;
+		return validate_url_redis(buf, len);
+	} else if (strncmp(buf, "rediss://", 9) == 0) {
+		printf("scheme: rediss\n");
+		buf += 9; len -= 9;
+		return validate_url_rediss(buf, len);
+	}
+
+	printf("%s is not a valid URL!\n", buf);
+	return 0;
 }
 
 static void redis_decode_test(char *url)
@@ -51,7 +73,7 @@ static void redis_decode_test(char *url)
 	if(len < 7) { return; }
 	char *buf = calloc(1, len+1);
 	memcpy(buf, url, len);
-	redis_url_decode(buf, len);
+	validate_url(buf, len);
 	free(buf);
 }
 
